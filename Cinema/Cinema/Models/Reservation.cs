9b47323d574984d52d1317677551b2861a363a8a
mowerczuk <maciej.owerczuk@gmail.com>
2017-01-17 14:51:30 +0100

    fixed models

--- Cinema/Cinema/Models/Reservation.cs
+++ Cinema/Cinema/Models/Reservation.cs
@@ -15,11 +15,12 @@ namespace Cinema.Models
     class Reservation
     {
         public int Id { get; set; }
-        public Show Show { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
-        public List<string> Seats { get; set; }
+        public string Seats { get; set; }
         public Ticket TicketType { get; set; }
         public bool WasPaid { get; set; }
+        public int ShowId { get; set; }
+        public virtual Show Show { get; set; }
     }
 }
--- Cinema/Cinema/Models/Show.cs
+++ Cinema/Cinema/Models/Show.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +10,12 @@ namespace Cinema.Models
     class Show
     {
         public int Id { get; set; }
-        public Movie Movie { get; set; }
-        public List<Reservation> Reservation { get; set; }
         public DateTime? ShowDate { get; set; }
         public TimeSpan StartTime { get; set; }
         public TimeSpan EndTime { get; set; }
         public int Hall { get; set; }
+        public int MovieId { get; set; }
+        public virtual Movie Movie { get; set; }
+        public virtual ObservableCollection<Reservation> Reservation { get; set; }
     }
 }
