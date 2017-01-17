3b771a4125d5f6ba2ceb7b72f0eb121299e6b1d2
mowerczuk <maciej.owerczuk@gmail.com>
2017-01-17 16:24:09 +0100

    added DbManager with singleton, added Add function

---
+++ Cinema/Cinema/ViewModels/DbManager.cs
@@ -0,0 +1,43 @@
+﻿using Cinema.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.ViewModels
+{
+    class DbManager
+    {
+        CinemaDbContext db = new CinemaDbContext();
+
+        private static DbManager _instance;
+
+        private DbManager() { }
+
+        public static DbManager GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new DbManager();
+            }
+            return _instance;
+        }
+
+        public void Add(Object toAdd)
+        {
+            if (toAdd is Movie)
+            {
+                db.Movies.Add(toAdd as Movie);
+            }
+            else if (toAdd is Reservation)
+            {
+                db.Reservations.Add(toAdd as Reservation);
+            }
+            else if (toAdd is Show)
+            {
+                db.Shows.Add(toAdd as Show);
+            }
+        }
+    }
+}
