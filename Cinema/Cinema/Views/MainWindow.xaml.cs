fda0d8d3c60d14864748a50f1db539474ba9c8a4
Kamil Kuryś <kkurys@10g.pl>
2017-01-17 17:49:14 +0100

    migrated views without logic from previous project version

--- Cinema/Cinema/Views/MainWindow.xaml.cs
+++ Cinema/Cinema/Views/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Cinema.Models;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,293 @@ namespace Cinema.Views
     /// </summary>
     public partial class MainWindow : Window
     {
+        CultureInfo cultureInfo = new CultureInfo("pl-PL");
         public MainWindow()
         {
             InitializeComponent();
         }
+        #region File Save&Load
+        public void LoadImages()
+        {
+           
+        }
+
+        public static void WriteToBinaryFile<T>(string filePath, T objectToWrite, bool append = false)
+        {
+            using (Stream stream = File.Open(filePath, append ? FileMode.Append : FileMode.Create))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                binaryFormatter.Serialize(stream, objectToWrite);
+            }
+        }
+
+        public static T ReadFromBinaryFile<T>(string filePath)
+        {
+            using (Stream stream = File.Open(filePath, FileMode.Open))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                return (T)binaryFormatter.Deserialize(stream);
+            }
+        }
+        #endregion
+        #region methods
+        private void ReservationSelected(object sender, SelectionChangedEventArgs e)
+        {
+   /*         if (LBReservedList.SelectedIndex > -1 && CBPaid.IsChecked == false)
+            {
+                GBReservationDetails.IsEnabled = true;
+            }
+            else
+            {
+                GBReservationDetails.IsEnabled = false;
+            }
+            */
+            //if (GBReservationDetails.IsEnabled == false && CBPaid.IsChecked == false)
+            //{
+            //    GBReservationDetails.IsEnabled = true;
+            //}
+            //else
+            //{
+            //    GBReservationDetails.IsEnabled = false;
+            //}
+        }
+        private void WasPaid(object sender, RoutedEventArgs e)
+        {
+            if (CBPaid != null && CBPaid.IsChecked == true)
+            {
+                CBPaid.IsEnabled = false;
+            }
+        }
+
+        private void Close(object sender, RoutedEventArgs e)
+        {
+           // WriteToBinaryFile<Database>("database.bin", db);
+            this.Close();
+        }
+
+        private void AddMovie(object sender, RoutedEventArgs e)
+        {
+    //        AddMovieWindow newMovie = new AddMovieWindow(db);
+     //       newMovie.Show();
+        }
+        /*
+        public int FindAvailableId()
+        {
+            int id = 1;
+            bool sw = true;
+            if (db.Reservations == null || db.Reservations.Count == 0)
+            {
+                sw = false;
+            }
+            while (sw)
+            {
+                foreach (Reservation r in db.Reservations)
+                {
+                    if (r.Id == id)
+                    {
+                        id++;
+                        sw = true;
+                        break;
+                    }
+                    sw = false;
+                }
+            }
+            return id;
+        } */
+        private void OpenShowsWindow(object sender, RoutedEventArgs e)
+        {
+        //    ShowsWindow shows = new ShowsWindow(db);
+
+            //shows.Show();
+        }
+        private void DisplayShowsForToday(object sender, RoutedEventArgs e)
+        {
+            /*
+            BTShowAll.Visibility = Visibility.Collapsed;
+            BTPrev.Visibility = Visibility.Visible;
+            BTNext.Visibility = Visibility.Visible;
+            LBCurrentDate.Visibility = Visibility.Visible;
+            ApplyDateFilter();
+            GroupHours();
+            TBGroupHeader.Text = "Wszystkie seanse"; */
+        }
+        private void KeyShortcuts(object sender, KeyEventArgs e)
+        {
+            /*
+            if (TCMainWindow.SelectedIndex == 1)
+            {
+                if (e.Key == Key.Left)
+                {
+                    if (BTPrev.IsEnabled == true)
+                    {
+                        //PrevWeek_Executed(null, null);
+                    }
+                    e.Handled = true;
+                }
+                if (e.Key == Key.Right)
+                {
+                    if (BTNext.IsEnabled == true)
+                    {
+                      //  NextWeek_Executed(null, null);
+                    }
+                    e.Handled = true;
+                }
+            } */
+        }
+        private void EditMovie(object sender, MouseButtonEventArgs e)
+        {
+          //  AddMovieWindow newMovie = new AddMovieWindow(LBRepertuar.SelectedItem as Movie, db);
+         //   newMovie.Show();
+        }
+        #endregion
+        #region groups&sorts methods
+        private ListCollectionView MoviesView
+        {
+            get
+            {
+                //        return (ListCollectionView)CollectionViewSource.GetDefaultView(db.Movies);
+                return null;
+            }
+        }
+        private void SortNone(object sender, RoutedEventArgs e)
+        {
+            /*
+            MoviesView.SortDescriptions.Clear();
+            */
+        }
+
+        private void SortTitle(object sender, RoutedEventArgs e)
+        {
+           // MoviesView.SortDescriptions.Clear();
+        //    MoviesView.SortDescriptions.Add(new SortDescription("Title", ListSortDirection.Ascending));
+        }
+
+        private void SortPremiereDate(object sender, RoutedEventArgs e)
+        {
+         //   MoviesView.SortDescriptions.Clear();
+         //   MoviesView.SortDescriptions.Add(new SortDescription("PremiereDate", ListSortDirection.Descending));
+        }
+        private void SortStartTime()
+        {
+           // ShowsView.SortDescriptions.Clear();
+          //  ShowsView.SortDescriptions.Add(new SortDescription("StartTime", ListSortDirection.Ascending));
+        }
+        private void SortShowDate()
+        {
+         //   ShowsView.SortDescriptions.Clear();
+         //   ShowsView.SortDescriptions.Add(new SortDescription("ShowDate", ListSortDirection.Ascending));
+        }
+        private void GroupNone(object sender, RoutedEventArgs e)
+        {
+          //  MoviesView.GroupDescriptions.Clear();
+        }
+
+        private void GroupGenre(object sender, RoutedEventArgs e)
+        {
+          //  MoviesView.GroupDescriptions.Clear();
+          //  MoviesView.GroupDescriptions.Add(new PropertyGroupDescription("Genre"));
+        }
+
+        private void GroupProduction(object sender, RoutedEventArgs e)
+        {
+           // MoviesView.GroupDescriptions.Clear();
+          //  MoviesView.GroupDescriptions.Add(new PropertyGroupDescription("Production"));
+        }
+        private void GroupDate()
+        {
+          //  ShowsView.GroupDescriptions.Clear();
+          //  ShowsView.GroupDescriptions.Add(new PropertyGroupDescription("ShortDate"));
+
+        }
+        private void GroupHours()
+        {
+         //   ShowsView.GroupDescriptions.Clear();
+      //      ShowsView.GroupDescriptions.Add(new PropertyGroupDescription("StartTime", showsGrouper));
+        }
+        #endregion
+        #region filters
+        public ListCollectionView ShowsView
+        {
+            get
+            {
+                //      return (ListCollectionView)CollectionViewSource.GetDefaultView(db.Shows);
+                return null;
+            }
+        }
+        private ListCollectionView ReservationsView
+        {
+            get
+            {
+         //       return (ListCollectionView)CollectionViewSource.GetDefaultView(db.Reservations);
+                return null;
+            }
+        }
+        private void RemoveFilter()
+        {
+        //    ShowsView.Filter = null;
+        }
+        private void ApplyDateFilter()
+        {
+       /*     var currentDate = DateTime.Today;
+            ShowsView.Filter = delegate (object item)
+            {
+                Show show = item as Show;
+                if (show.ShowDate == currentDate)
+                {
+                    return true;
+                }
+                return false;
+            }; */
+        }
+        private void ApplyTitleFilter()
+        {
+            /*
+            ShowsView.Filter = delegate (object item)
+            {
+                Show show = item as Show;
+                if (show.Movie.Title == (LBRepertuar.SelectedItem as Movie).Title)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            };
+            */
+        }
+        private void ReservFilterChanged(object sender, TextChangedEventArgs e)
+        {
+            /*
+            ReservationsView.Filter = delegate (object item)
+            {
+                Reservation res = item as Reservation;
+                if (TBId.Text != "")
+                {
+                    int id;
+                    if (int.TryParse(TBId.Text, out id))
+                    {
+                        if (cultureInfo.CompareInfo.IndexOf(res.Name, TBResName.Text, CompareOptions.IgnoreCase) >= 0 && cultureInfo.CompareInfo.IndexOf(res.Surname, TBResSurname.Text, CompareOptions.IgnoreCase) >= 0 && res.Id == id)
+                        {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+                else
+                {
+                    if (cultureInfo.CompareInfo.IndexOf(res.Name, TBResName.Text, CompareOptions.IgnoreCase) >= 0 && cultureInfo.CompareInfo.IndexOf(res.Surname, TBResSurname.Text, CompareOptions.IgnoreCase) >= 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }; */
+        }
+        #endregion
+
     }
 }
---
+++ Cinema/Cinema/Views/ShowsWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cinema.Views
+{
+    /// <summary>
+    /// Interaction logic for ShowsWindow.xaml
+    /// </summary>
+    public partial class ShowsWindow : Window
+    {
+        public ShowsWindow()
+        {
+            InitializeComponent();
+        }
+        private void GenerateTable(object sender, RoutedEventArgs e)
+        {
+            /*
+            if (CBMovies.SelectedIndex == -1 || CBHalls.SelectedIndex == -1) return;
+            Filter();
+            TurnAllButtonsOn();
+            Button btn;
+            TimeSpan tempStartTime = new TimeSpan(10, 0, 0);
+            TimeSpan tempEndTime = tempStartTime.Add(new TimeSpan(0, (CBMovies.SelectedItem as Movie).Length, 0)).Add(new TimeSpan(0, 30, 0));
+            int row;
+            bool cont = false;
+            for (int day = 0; day < 7; day++)
+            {
+                tempStartTime = new TimeSpan(10, 0, 0);
+                tempEndTime = tempStartTime.Add(new TimeSpan(0, (CBMovies.SelectedItem as Movie).Length, 0));
+                row = 0;
+                while (tempStartTime.CompareTo(new TimeSpan(22, 30, 0)) < 0)
+                {
+                    cont = false;
+                    btn = LogicalTreeHelper.FindLogicalNode(DataGrid, "btn" + row + "" + day) as Button;
+                    foreach (Show s in showsToAdd)
+                    {
+                        if (!s.ShowDate.Equals(dt.AddDays(day)))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            int hall;
+                            if (int.TryParse(CBHalls.SelectedItem as string, out hall))
+                            {
+                                if (hall == s.Hall && ((tempEndTime.CompareTo(s.StartTime) >= 0 && tempEndTime.CompareTo(s.EndTime) <= 0) ||
+                                    (tempStartTime.CompareTo(s.StartTime) >= 0 && tempStartTime.CompareTo(s.EndTime) <= 0)))
+                                {
+                                    cont = true;
+                                    break;
+                                }
+                            }
+
+                        }
+                    }
+
+                    if (cont)
+                        btn.IsEnabled = false;
+
+                    row++;
+                    tempStartTime = tempStartTime.Add(new TimeSpan(0, 30, 0));
+                    tempEndTime = tempEndTime.Add(new TimeSpan(0, 30, 0));
+                }
+            }
+            */
+        }
+
+        private void TurnAllButtonsOn()
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 25; j++)
+                {
+                    Button btn = LogicalTreeHelper.FindLogicalNode(DataGrid, "btn" + j + "" + i) as Button;
+                    btn.IsEnabled = true;
+                }
+            }
+        }
+        private void FinishEditing(object sender, RoutedEventArgs e)
+        {
+            /*
+            View.Filter = null;
+            foreach (Show s in showsToRemove)
+            {
+                if (data.Shows.Contains(s))
+                {
+                    data.Shows.Remove(s);
+                    s.Movie.NumberOfShows--;
+                }
+            }
+            foreach (Show s in showsToAdd)
+            {
+                if (!data.Shows.Contains(s))
+                {
+                    data.Shows.Add(s);
+                    s.Movie.NumberOfShows++;
+                }
+            } */
+            Close();
+        }
+        private void KeyShortcuts(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Left)
+            {
+                if (BTPrev.IsEnabled == true)
+                {
+                 //   PrevWeek_Executed(null, null);
+                }
+            }
+            if (e.Key == Key.Right)
+            {
+                if (BTNext.IsEnabled == true)
+                {
+                //    NextWeek_Executed(null, null);
+                }
+            }
+            e.Handled = true;
+        }
+
+        private void CancelButtonClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        public void Add_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+
+        }
+        public void Add_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+        }
+    }
+}
