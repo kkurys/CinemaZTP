50ba8e5980c04fc34e0e594ac4f0549fa1e26aab
boarix <jakub_dziegielewski@o2.pl>
2017-01-17 18:05:48 +0100

    add folder "TicketBuilder" and model "ITicket"

---
+++ Cinema/Cinema/Models/ITicket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.Models
+{
+    public abstract class ITicket
+    {
+        private double price;
+        private string seats;
+        private Cinema.Models.Show show;
+
+        public ITicket getTicket(){ return this; }
+        public void setPrice(double price) { this.price = price; }
+        public void setSeats(string seats) { this.seats = seats; }
+        public void setShow(Cinema.Models.Show show) { this.show = show; }
+        public double getPrice() { return this.price; }
+        public string getSeats() { return this.seats; }
+        public Cinema.Models.Show getShow() { return this.show; }
+    }
+}
