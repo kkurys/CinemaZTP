b4f0ae1f6605bd0421a5e950348a39e72ee3c6b7
mowerczuk <maciej.owerczuk@gmail.com>
2017-01-17 14:25:59 +0100

    mvvm structure

--- Cinema/Cinema/Views/MainWindow.xaml.cs
+++ Cinema/Cinema/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Cinema
+namespace Cinema.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
