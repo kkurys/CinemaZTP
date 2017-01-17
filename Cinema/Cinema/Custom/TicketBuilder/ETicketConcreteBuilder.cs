43ada5c70128f9f3f79076a013884018b2455cbb
Kamil Kuryś <kkurys@10g.pl>
2017-01-17 18:51:20 +0100

    moved files to adequate folders

---
+++ Cinema/Cinema/Custom/TicketBuilder/ETicketConcreteBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.TicketBuilder
+{
+    class ETicketConcreteBuilder : Cinema.TicketBuilder.ITicketBuilder
+    {
+        public Cinema.Models.ITicket BuildTicket(Cinema.Models.Reservation reservation)
+        {
+            try
+            {
+                return new Cinema.TicketBuilder.ETicket(reservation);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Cinema/Cinema/Custom/TicketBuilder/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.TicketBuilder
+{
+    class Ticket : Cinema.Models.ITicket
+    {
+        private Ticket() {}
+        public Ticket(Cinema.Models.Reservation reservation)
+        {
+            this.setShow(reservation.Show);
+            this.setSeats(reservation.Seats);
+            // this.setPrice(29.99);
+        }
+    }
+}
